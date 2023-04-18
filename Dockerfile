36c7d82da4f59c3d155b91d5831367688c01d63d
juanluna96 <juandluna17@gmail.com>
2023-04-17 22:16:04 -0500

    Serverless webpack

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ FROM node:16.20.0-alpine3.16
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
-COPY . .
+
+COPY serverless.yml ./
+COPY src ./src
+COPY webpack.config.js ./
 
 EXPOSE 3000
 EXPOSE 3002
--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,14 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
+const hello = async (event) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Old log message");
+  // console.log("New log message");
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
@@ -16,3 +19,5 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.handler = hello;
